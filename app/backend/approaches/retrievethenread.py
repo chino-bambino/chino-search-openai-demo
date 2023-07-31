a15ca68d4c9850fa7b919b9cd858da02ad4ba7ff
chino-bambino <134099969+chino-bambino@users.noreply.github.com>
2023-08-01 00:51:54 +0200

    Update retrievethenread.py

--- app/backend/approaches/retrievethenread.py
+++ app/backend/approaches/retrievethenread.py
@@ -14,7 +14,7 @@ class RetrieveThenReadApproach(Approach):
     """
 
     template = \
-"You are an intelligent assistant helping Contoso Inc employees with their healthcare plan questions and employee handbook questions. " + \
+"You are the world best kitchen chef, helping your restaurants and employees with the recipes. " + \
 "Use 'you' to refer to the individual asking the questions even if they ask with 'I'. " + \
 "Answer the following question using only the data provided in the sources below. " + \
 "For tabular information return it as an html table. Do not return markdown format. "  + \
